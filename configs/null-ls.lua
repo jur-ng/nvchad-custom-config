b235fa14a43e4c858f7c6815db67ec05a099a57a
jur <jur@nextgear.nl>
2023-11-07 17:08:11 +0100

    Fix CSS auto sort using postcss

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -2,12 +2,27 @@ local null_ls = require "null-ls"
 
 local b = null_ls.builtins
 
+local postcss_formatter = {
+    name = "postcss",
+    method = null_ls.methods.FORMATTING,
+    filetypes = { "css" },
+    generator = null_ls.generator({
+        command = "postcss",
+        args = { "--stdin" },
+        to_stdin = true,
+        on_output = function(params, done)
+            done({ { text = params.output } })
+        end,
+    }),
+}
+
+
 local sources = {
 
   -- webdev stuff
   b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
-
+  b.formatting.prettier.with { filetypes = { "html", "markdown" } }, -- so prettier works only on these filetypes,
+  postcss_formatter,
   -- Lua
   b.formatting.stylua,
 
